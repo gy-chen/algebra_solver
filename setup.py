d88becb5b2af68bd6d4b942c9216efdec32f9d70
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-12-18 15:29:29 +0800

    init

---
+++ setup.py
@@ -0,0 +1,28 @@
+from io import open
+
+from setuptools import find_packages, setup
+
+with open('algebra_solver/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
+
+
+REQUIRES = []
+
+setup(
+    name='algebra_solver',
+    version=version,
+    description='',
+    author='GYCHEN',
+    author_email='gy.chen@gms.nutc.edu.tw',
+    url='https://github.com/gy-chen/algebra_solver',
+
+    install_requires=REQUIRES,
+    tests_require=['coverage', 'pytest'],
+
+    packages=find_packages(),
+)
