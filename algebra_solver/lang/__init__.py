d1946fe781b274be71a7700e84604910f5f01398
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-12-19 20:40:24 +0800

    create lexer

---
+++ algebra_solver/lang/__init__.py
@@ -0,0 +1,8 @@
+"""Provide tools for build syntax tree of algebra equation.
+
+lexer
+  tokenize algebra equation.
+
+parser
+  parse tokens of algebra equation
+"""
---
+++ algebra_solver/lang/lexer.py
@@ -0,0 +1,100 @@
+"""provide tools for tokenize algebra equation
+
+tokenize(text)
+  main function to tokenize albebra equation. it's a generator that generate tokens.
+
+TokenType
+  enum that records recognized token types.
+
+Token
+  nametuple that keep token's value and it's type
+"""
+import string
+import io
+from itertools import chain
+from enum import Enum
+from collections import namedtuple
+
+
+class TokenType(Enum):
+    IDENTIFIER = 'IDENTIFIER'
+    NUMBER = 'NUMBER'
+    PLUS = 'PLUS'
+    MINUS = 'MINUS'
+    MULTIPLE = 'MULTIPLE'
+    DIVIDE = 'DIVIDE'
+    L_PAREN = 'L_PAREN'
+    R_PAREN = 'R_PAREN'
+    EQUATION = 'EQUATION'
+
+
+CHAR_TO_TYPES = {
+    '+': TokenType.PLUS,
+    '-': TokenType.MINUS,
+    '*': TokenType.MULTIPLE,
+    '/': TokenType.DIVIDE,
+    '(': TokenType.L_PAREN,
+    ')': TokenType.R_PARAN,
+    '=': TokenType.EQUATION
+}
+
+
+Token = namedtuple('Token', 'type value position')
+
+
+class UnknownTokenError(Exception):
+
+    def __init__(self, position, message=None):
+        super(message)
+        self.position = position
+
+
+def tokenize(text):
+    gen = enumerate(text)
+    for p, c in gen:
+        if c.isspace():
+            continue
+        if c in CHAR_TO_TYPES:
+            yield Token(CHAR_TO_TYPES[c], c, p)
+        elif c.isnumeric():
+            token, gen = _read_number(c, p, gen)
+            yield token
+        elif c.isalpha():
+            token, gen = _read_identifier(c, p, gen)
+            yield token
+        else:
+            raise UnknownTokenError(p)
+
+
+def _read_number(c, p, gen):
+    "return (token, gen)"
+    expected = set(string.digits) | set('.')
+    fp = p
+    value = io.StringIO()
+    while c in expected:
+        value.write(c)
+        if c == '.':
+            expected.remove('.')
+        try:
+            c, p = next(gen)
+        except StopIteration:
+            break
+    else:
+        gen = chain([(p, c)], gen)
+    return Token(TokenType.NUMBER, float(value.getvalue()), fp), gen
+
+
+def _read_identifier(c, p, gen):
+    "return (token, gen)"
+    expected = string.ascii_letters
+    fp = p
+    value = io.StringIO()
+    while c in expected:
+        value.write(c)
+        try:
+            c, p = next(gen)
+        except StopIteration:
+            break
+    else:
+        gen = chain([(p, c)], gen)
+    return Token(TokenType.IDENTIFIER, value.getvalue(), fp), gen
