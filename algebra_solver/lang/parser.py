d5460300713f6518ba88616f6b062eda51287bbe
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-12-21 18:10:42 +0800

    add parser

---
+++ algebra_solver/lang/parser.py
@@ -0,0 +1,151 @@
+"""provide tools for parse syntax of algebra equation
+
+parse
+    main function for parsing algebra equation, return EquationExpression
+
+EquationExpression
+    namedtuple that represent albegra equation
+
+InfixExpression
+    namedtuple that represent expression that have operator and two operands on left and right.
+
+NumberExpression
+    namedtuple that represent expression that only have a number as its value.
+
+IdentifierExpression
+    namedtuple that represent expression that only have a idenfiier as its value.
+
+OperatorType
+    enum that represent types of operator
+
+ParseException
+    root exception for paring error
+
+UnexpectTokenException
+    exception for unexpect token
+"""
+from enum import Enum
+from collections import namedtuple
+from itertools import chain
+from . import lexer
+
+
+class ParseException(Exception):
+    pass
+
+
+class UnexceptEndException(ParseException):
+    pass
+
+
+class UnexpectTokenException(ParseException):
+
+    def __init__(self, token):
+        self.token = token
+
+
+EquationExpression = namedtuple('EquationExpression', 'left right')
+InfixExpression = namedtuple('InfixExpression', 'operator left right')
+NumberExpression = namedtuple('NumberExpression', 'number')
+IdentifierExpression = namedtuple('IdentifierExpression', 'identifier')
+
+
+class OperatorType(Enum):
+    LOWEST = 'LOWEST'
+    PLUS = 'PLUS'
+    MINUS = 'MINUS'
+    MULTIPLE = 'MULTIPLE'
+    DIVIDE = 'DIVIDE'
+    L_PAREN = 'L_PAREN'
+
+
+TOKEN_TYPE_TO_OPERATOR_TYPE = {
+    lexer.TokenType.PLUS: OperatorType.PLUS,
+    lexer.TokenType.MINUS: OperatorType.MINUS,
+    lexer.TokenType.MULTIPLE: OperatorType.MULTIPLE,
+    lexer.TokenType.DIVIDE: OperatorType.DIVIDE
+}
+
+PRECEDENCE = {
+    OperatorType.LOWEST: 0,
+    OperatorType.L_PAREN: 1,
+    OperatorType.PLUS: 2,
+    OperatorType.MINUS: 2,
+    OperatorType.MULTIPLE: 3,
+    OperatorType.DIVIDE: 4
+}
+
+
+def parse(tokens_gen):
+    equation_expression = _parse_equation_expression(tokens_gen)
+    try:
+        token = next(tokens_gen)
+        raise UnexpectTokenException(token)
+    except StopIteration:
+        pass
+    return equation_expression
+
+
+def _parse_equation_expression(tokens_gen):
+    left, tokens_gen = _parse_expression(tokens_gen)
+    try:
+        token = next(tokens_gen)
+        if token.type != lexer.TokenType.EQUATION:
+            raise UnexpectTokenException(token)
+    except StopIteration:
+        raise UnexceptEndException()
+    right, tokens_gen = _parse_expression(tokens_gen)
+    return EquationExpression(left, right)
+
+
+def _parse_expression(tokens_gen, precedence=PRECEDENCE[OperatorType.LOWEST]):
+    left = _parse_operand(tokens_gen)
+    try:
+        token = next(tokens_gen)
+    except StopIteration:
+        return left, tokens_gen
+    operator = TOKEN_TYPE_TO_OPERATOR_TYPE.get(token.type, None)
+    if operator is None:
+        tokens_gen = chain([token], tokens_gen)
+        return left, tokens_gen
+
+    while precedence < PRECEDENCE[operator]:
+        right, tokens_gen = _parse_expression(tokens_gen, PRECEDENCE[operator])
+        try:
+            token = next(tokens_gen)
+        except StopIteration:
+            return left, tokens_gen
+        left = InfixExpression(operator, left, right)
+        operator = TOKEN_TYPE_TO_OPERATOR_TYPE.get(token.type, None)
+        if operator is None:
+            tokens_gen = chain([token], tokens_gen)
+            return left, tokens_gen
+    tokens_gen = chain([token], tokens_gen)
+    return left, tokens_gen
+
+
+def _parse_operand(tokens_gen):
+    try:
+        token = next(tokens_gen)
+    except StopIteration:
+        raise UnexceptEndException()
+    if token.type == lexer.TokenType.NUMBER:
+        return NumberExpression(token.value)
+    elif token.type == lexer.TokenType.IDENTIFIER:
+        return IdentifierExpression(token.value)
+    elif token.type == lexer.TokenType.L_PAREN:
+        return _parse_paren_expression(tokens_gen)
+    else:
+        raise UnexpectTokenException(token)
+
+
+def _parse_paren_expression(token_gens):
+    exp, token_gens = _parse_expression(
+        token_gens, PRECEDENCE[OperatorType.L_PAREN])
+    try:
+        token = next(token_gens)
+    except StopIteration:
+        raise UnexceptEndException()
+    if token.type != lexer.TokenType.R_PAREN:
+        raise UnexpectTokenException(token)
+    return exp
---
+++ tests/test_parser.py
@@ -0,0 +1,78 @@
+import pytest
+from algebra_solver.lang import lexer
+from algebra_solver.lang import parser
+
+
+def test_simple_parse():
+    tokens = lexer.tokenize('x + 3 = 10')
+    equation_expression = parser.parse(tokens)
+    assert isinstance(equation_expression, parser.EquationExpression)
+    assert isinstance(equation_expression.left, parser.InfixExpression)
+    assert isinstance(equation_expression.right, parser.NumberExpression)
+
+    assert equation_expression.left.operator == parser.OperatorType.PLUS
+    assert equation_expression.left.left == parser.IdentifierExpression('x')
+    assert equation_expression.left.right == parser.NumberExpression(3)
+
+    assert equation_expression.right == parser.NumberExpression(10)
+
+
+def test_parse_expression():
+    plus_expression = parser.parse(lexer.tokenize('1+1=1'))
+    assert plus_expression.left.operator == parser.OperatorType.PLUS
+
+    minus_expression = parser.parse(lexer.tokenize('1-1=1'))
+    assert minus_expression.left.operator == parser.OperatorType.MINUS
+
+    multiple_expression = parser.parse(lexer.tokenize('1*1=1'))
+    assert multiple_expression.left.operator == parser.OperatorType.MULTIPLE
+
+    divide_expression = parser.parse(lexer.tokenize('1/1=1'))
+    assert divide_expression.left.operator == parser.OperatorType.DIVIDE
+
+
+def test_incomplete_equation():
+    with pytest.raises(parser.UnexceptEndException):
+        parser.parse(lexer.tokenize('x + 3'))
+
+
+def test_surplus_equation():
+    with pytest.raises(parser.UnexpectTokenException):
+        parser.parse(lexer.tokenize('x + 3 = 10 + 3 = 10'))
+
+
+def test_inorder_equation():
+    with pytest.raises(parser.UnexpectTokenException):
+        parser.parse(lexer.tokenize('x x + 3 = 10'))
+
+    with pytest.raises(parser.UnexpectTokenException):
+        parser.parse(lexer.tokenize('x + + 3 = 10'))
+
+    with pytest.raises(parser.UnexpectTokenException):
+        parser.parse(lexer.tokenize('x + 3 3 = 10'))
+
+    with pytest.raises(parser.UnexpectTokenException):
+        parser.parse(lexer.tokenize('x + 3 3 = =  10'))
+
+    with pytest.raises(parser.UnexpectTokenException):
+        parser.parse(lexer.tokenize('x + 3 3 = 10 10'))
+
+
+def test_precedence():
+    equation = parser.parse(lexer.tokenize('1 * 2 + x = 10'))
+
+    plus_exp = equation.left
+    assert plus_exp.operator == parser.OperatorType.PLUS
+    assert plus_exp.left == parser.InfixExpression(
+        parser.OperatorType.MULTIPLE, parser.NumberExpression(1), parser.NumberExpression(2))
+    assert plus_exp.right == parser.IdentifierExpression('x')
+
+
+def test_paren_precedence():
+    equation = parser.parse(lexer.tokenize('1 * (2 + x) = 10'))
+
+    multiple_exp = equation.left
+    assert multiple_exp.operator == parser.OperatorType.MULTIPLE
+    assert multiple_exp.left == parser.NumberExpression(1)
+    assert multiple_exp.right == parser.InfixExpression(
+        parser.OperatorType.PLUS, parser.NumberExpression(2), parser.IdentifierExpression('x'))
