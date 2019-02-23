133a622c09b0e84828af5513a69d6ca2518e4c7e
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-02-23 20:57:19 +0800

    organize code a little bit

--- algebra_solver/tf.py
+++ algebra_solver/tf.py
@@ -17,7 +17,8 @@ def eval(equation_expression, steps=50000):
 
     :return dict: contain key as algebra variables, and value as result of the variable.
     """
-    with tf.Graph().as_default():
+    graph = tf.Graph()
+    with graph.as_default():
         context = {}
         left = _eval(equation_expression.left, context)
         right = _eval(equation_expression.right, context)
@@ -26,20 +27,20 @@ def eval(equation_expression, steps=50000):
         optimizer = tf.train.AdamOptimizer()
         train = optimizer.minimize(loss)
 
-        result = {}
-        with tf.Session() as sess:
-            init = tf.global_variables_initializer()
-            sess.run(init)
+    result = {}
+    with tf.Session(graph=graph) as sess:
+        init = tf.global_variables_initializer()
+        sess.run(init)
 
-            for _ in range(steps):
-                sess.run(train)
+        for _ in range(steps):
+            sess.run(train)
 
-            for var in context:
-                result[var] = sess.run(context[var])
+        for var in context:
+            result[var] = sess.run(context[var])
 
-            result['_loss'] = sess.run(loss)
+        result['_loss'] = sess.run(loss)
 
-        return result
+    return result
 
 
 @singledispatch
@@ -68,7 +69,8 @@ def _(identifier_expression, context):
     """
     if identifier_expression.identifier in context:
         return context[identifier_expression.identifier]
-    tf_variable = tf.get_variable(identifier_expression.identifier, dtype=tf.float32, shape=(), initializer=tf.initializers.random_normal())
+    tf_variable = tf.get_variable(identifier_expression.identifier, dtype=tf.float32, shape=(
+    ), initializer=tf.initializers.random_normal())
     context[identifier_expression.identifier] = tf_variable
     return tf_variable
 
