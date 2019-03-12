14f02cde0aa4ee71da318ee69e09be69dbf95025
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-03-12 17:05:39 +0800

    implement epic detail

--- web/src/store/task/epics.ts
+++ web/src/store/task/epics.ts
@@ -1,18 +1,25 @@
 import { ofType, ActionsObservable, StateObservable, combineEpics } from 'redux-observable';
 import { Observable, Observer } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { PollTaskAction, SubmitTaskAction, SUBMIT_TASK, POLL_TASK, UpdateTaskAction, TaskEpicDependencies } from './types';
+import { switchMap, map } from 'rxjs/operators';
+import { pollTask, updateTask } from './actions';
+import { PollTaskAction, SubmitTaskAction, SUBMIT_TASK, POLL_TASK, UpdateTaskAction, TaskEpicDependencies, Task } from './types';
 import { RootState } from '../types';
 
-export const submitTaskEpic = (action$: ActionsObservable<SubmitTaskAction>, state$: StateObservable<RootState>, { taskApi }: TaskEpicDependencies) => action$.pipe(
+export const submitTaskEpic = (action$: ActionsObservable<SubmitTaskAction>, state$: StateObservable<RootState> | null, { taskApi }: TaskEpicDependencies) => action$.pipe(
     ofType(SUBMIT_TASK),
     switchMap((action): Observable<PollTaskAction> => {
         return Observable.create(
             (observer: Observer<PollTaskAction>) => {
-                // TODO call createTask api
-                // TODO after createTask success, emit PollTaskAction, and finished observable.
-                // TODO remove me later
-                observer.complete();
+                taskApi.createTask(action.payload.content)
+                    .then(
+                        res => {
+
+                            const task = res.data.task;
+                            action.payload.history.push(`/task/${task.id}`);
+                            observer.next(pollTask(task.id));
+                        }
+                    )
+                    .finally(() => observer.complete());
             }
         );
     })
@@ -21,14 +28,8 @@ export const submitTaskEpic = (action$: ActionsObservable<SubmitTaskAction>, sta
 export const pollTaskEpic = (action$: ActionsObservable<PollTaskAction>, state$: StateObservable<RootState>, { taskApi }: TaskEpicDependencies) => action$.pipe(
     ofType(POLL_TASK),
     switchMap((action): Observable<UpdateTaskAction> => {
-        return Observable.create(
-            (observer: Observer<UpdateTaskAction>) => {
-                // TODO poll through websocket
-                // TODO keep update from data received from websocket
-                // TODO complte observer when websocket is closed
-                // TODO remove me later
-                observer.complete();
-            }
+        return taskApi.pollTask(action.payload.id).pipe(
+            map((task: Task) => updateTask(task))
         );
     })
 );
