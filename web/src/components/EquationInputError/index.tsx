6d40742e3259320cd3f59231d23841aef8bf30a5
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-03-18 10:10:09 +0800

    implement EquationInputError component

--- web/src/components/EquationInputError/index.tsx
+++ web/src/components/EquationInputError/index.tsx
@@ -1,4 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+import { EquationInputErrorProps } from './types';
+import { TaskContentErrorType } from '../../store/task/types';
+
+const Container = styled.div``;
+
+const ErrorMessage = styled.div`
+    font-family: monospace;
+    color: red;
+    font-weight: bold;
+`;
+
+interface PositionErrorProps {
+    position: number
+}
+
+const PositionError = styled.div`
+    :before {
+        font-family: monospace;
+        content: '>';
+        display: inline-block;
+        transform: rotate(-90deg) translateY(${(p: PositionErrorProps) => p.position}ch);
+    }
+`;
+
 
 /**
  * EquationInputError
@@ -6,8 +31,37 @@ import React from 'react';
  * props:
  *   - error: TaskContentError
  */
-const EquationInputError = () => {
-    return <div>EquationInputError</div>;
+const EquationInputError = (props: EquationInputErrorProps) => {
+    const { error } = props;
+    return (
+        <Container>
+            {renderPositionError(error.position)}
+            {renderErrorMessage(error.type, error.token)}
+        </Container>
+    );
+}
+
+const errorMessages = {
+    [TaskContentErrorType.EMPTY]: 'Please input equation',
+    [TaskContentErrorType.UNEXPECTED_END]: 'Please complete your equation',
+    [TaskContentErrorType.UNEXPECTED_TOKEN]: 'Unexpected token: ',
+    [TaskContentErrorType.UNKNOWN_TOKEN]: 'Unknown token',
+    [TaskContentErrorType.OTHER]: 'Error'
+};
+
+const renderErrorMessage = (errorType: TaskContentErrorType, token: string | null) => {
+    let errorMessage = errorMessages[errorType];
+    if (token !== null) {
+        errorMessage = errorMessage + token;
+    }
+    return <ErrorMessage>{errorMessage}</ErrorMessage>;
+}
+
+const renderPositionError = (position: number | null) => {
+    if (position === null) {
+        return null;
+    }
+    return <PositionError position={position} />;
 }
 
 export default EquationInputError;
---
+++ web/src/components/EquationInputError/types.tsx
@@ -0,0 +1,5 @@
+import { TaskContentError } from '../../store/task/types';
+
+export interface EquationInputErrorProps {
+    error: TaskContentError
+};
