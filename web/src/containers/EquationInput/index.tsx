ca0168944f03895aebeb859b365ce82973e025fd
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-03-18 15:04:05 +0800

    display equation input error

--- web/src/containers/EquationInput/index.tsx
+++ web/src/containers/EquationInput/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { withRouter } from 'react-router-dom';
@@ -7,30 +8,54 @@ import { submitTask } from '../../store/task/actions';
 import EquationInput from '../../components/EquationInput';
 import { RootState } from '../../store/types';
 import { EquationInputState } from '../../components/EquationInput/types';
-import { TaskState } from '../../store/task/types';
+import { TaskState, TaskContentError } from '../../store/task/types';
+import EquationInputError from '../../components/EquationInputError';
 
 interface EquationInputContainerProps {
     dispatch: Dispatch,
     history: History,
-    state: EquationInputState
+    state: EquationInputState,
+    error: TaskContentError | null
 }
 
 const mapStateToProps = (state: RootState) => {
     return {
-        state: state.task.state === TaskState.SUBMITTING ? EquationInputState.SUBMITTING : EquationInputState.NORMAL
+        state: state.task.state === TaskState.SUBMITTING ? EquationInputState.SUBMITTING : EquationInputState.NORMAL,
+        error: state.task.error
     };
 };
 
-const EuqationInputContainer = ({ history, dispatch, ...props }: EquationInputContainerProps) => {
+const EquationInputErrorContainer = styled.div`
+    padding-left: 15px;
+`;
+
+
+const EuqationInputContainer = ({ history, dispatch, error, ...props }: EquationInputContainerProps) => {
 
     const onSubmit = (content: string) => {
         dispatch(submitTask(content, history));
     }
 
-    return <EquationInput
-        {...props}
-        onSubmit={onSubmit}
-    />;
+    return (
+        <div>
+            <EquationInput
+                {...props}
+                onSubmit={onSubmit}
+            />
+            {renderEquationInputError(error)}
+        </div>
+    );
+}
+
+const renderEquationInputError = (error: TaskContentError | null) => {
+    if (error === null) {
+        return null;
+    }
+    return (
+        <EquationInputErrorContainer>
+            <EquationInputError error={error} />
+        </EquationInputErrorContainer>
+    );
 }
 
 export default withRouter(connect(mapStateToProps, null)(EuqationInputContainer));
